02ed4d2236ad262919f8607dec981861284162af
scott sheble <117122862+scottsheble@users.noreply.github.com>
2023-08-30 11:55:02 -0500

    Initial variables and user input created

--- Assignment 2/main.cpp
+++ Assignment 2/main.cpp
@@ -1,14 +1,35 @@
-//
-//  main.cpp
-//  Assignment 2
-//
-//  Created by scott sheble on 8/30/23.
-//
+/********************************************************
+    CSCI 240        Program 2       Fall 2023
+
+    Programmer: Scott Sheble
+
+    Section: 1
+
+    Date Due: 9/15/2023
+
+    Purpose: Compiler Training
+
+    Created by scott sheble on 8/28/23.
+*********************************************************/
 
 #include <iostream>
+#include <iomanip>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int maxSquat = 0;
+int maxBench = 0;
+int maxDeadlift = 0;
+
+
+int main()
+{
+    
+    std::cout << "What is your max squat?";
+    std::cin >> maxSquat;
+    std::cout << "What is your max bench press?";
+    std::cin >> maxBench;
+    std::cout << "What is your max deadlift?";
+    std::cin >> maxDeadlift;
     return 0;
 }
