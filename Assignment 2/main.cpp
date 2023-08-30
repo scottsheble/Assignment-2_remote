cc32b288ce80edc18d72531da309ca644cbabdc0
scott sheble <117122862+scottsheble@users.noreply.github.com>
2023-08-30 13:11:32 -0500

    Add constants and align collumns and cout alignment

--- Assignment 2/main.cpp
+++ Assignment 2/main.cpp
@@ -14,22 +14,37 @@
 
 #include <iostream>
 #include <iomanip>
-//test
+
 using namespace std;
 
 int maxSquat = 0;
 int maxBench = 0;
 int maxDeadlift = 0;
+int powerliftingTotal = 0;
+static int leftColumnSpaces = 20;
+static int rightColumnSpaces = 8;
+static float wilksCoefficient = 0.5976;
+float wilksScore = 0;
 
 
 int main()
 {
     
-    std::cout << "What is your max squat?";
+    std::cout << "What is your max squat? ";
     std::cin >> maxSquat;
-    std::cout << "What is your max bench press?";
+    std::cout << "What is your max bench press? ";
     std::cin >> maxBench;
-    std::cout << "What is your max deadlift?";
+    std::cout << "What is your max deadlift? ";
     std::cin >> maxDeadlift;
+    powerliftingTotal = (maxSquat + maxBench + maxDeadlift);
+    wilksScore = powerliftingTotal * wilksCoefficient;
+    std::cout << endl;
+    std::cout << left << setw(leftColumnSpaces) << "Max Squat: " << right << setw(rightColumnSpaces)  << maxSquat << std::endl;
+    std::cout << left << setw(leftColumnSpaces) << "Max Bench Press: " << right << setw(rightColumnSpaces)  << maxBench << std::endl;
+    std::cout << left << setw(leftColumnSpaces) << "Max Deadlift: " << right << setw(rightColumnSpaces)  << maxDeadlift << std::endl;
+    std::cout << endl;
+    std::cout << left << setw(leftColumnSpaces) << "Powerlifting Total: " << right << setw(rightColumnSpaces)  << powerliftingTotal << std::endl;
+    std::cout << left << setw(leftColumnSpaces) << "Wilks Score: " << right << setw(rightColumnSpaces) << fixed << setprecision(3) << wilksScore << std::endl;
+    std::cout << endl;
     return 0;
 }
